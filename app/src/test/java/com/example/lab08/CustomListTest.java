c5a000afd7fa964f019b7c8c96a2b70c43f648cc
ADS2001 <arunavodutta21@gmail.com>
2025-10-28 17:25:23 -0600

    Error!

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,35 @@
 package com.example.lab08;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
 public class CustomListTest {
+
+        private CustomList list;
+
+        @BeforeEach
+        public void createList() {
+            list = new CustomList(null, new ArrayList<>());
+        }
+
+        @Test
+        public void hasCityTest() {
+            City city = new City("Edmonton", "AB");
+            list.addCity(city);
+            Assert.assertTrue(list.hasCity(city)); // Asserts that the supplied condition is true. [1, 2]
+        }
+
+        @Test
+        public void hasCityFalseTest() {
+            City city = new City("Edmonton", "AB");
+            City otherCity = new City("Vancouver", "BC");
+            list.addCity(city);
+            Assert.assertFalse(list.hasCity(otherCity)); // Asserts that the supplied condition is false. [1, 2]
+        }
+
 
 }
